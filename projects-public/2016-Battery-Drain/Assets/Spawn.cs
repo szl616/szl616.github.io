7c2c70bc36b7e8a51e7e7a273286dd87340b56ee
Mark Grøning Hansen <marjk9004@gmail.com>
2019-10-18 01:02:47 +0200

    Uploaded Projects

---
+++ projects-public/2016-Battery-Drain/Assets/Spawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+    public GameObject object1;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Instantiate(object1, this.transform.position, Quaternion.identity);
+    }
+}
