d152fe2acd3d7c1fbf97bfe1721581f83017c1b2
Mark Grøning Hansen <marjk9004@gmail.com>
2019-10-17 22:28:58 +0200

    Uploaded Project

---
+++ projects-public/2015-TowerDefense/Assets/Script/Turret.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+	
+	//Variabler start--------------------------------------------------------
+	public GameObject myProjectile;
+	
+	public float reloadTime = 1f;
+	public float turnSpeed = 5f;	
+	public float firePauseTime = 25f;	
+	public GameObject muzzleEffect;	
+	public float errorAmount = 001f;	
+	public Transform myTarget;	
+	public Transform[] muzzlePositions;	
+	public Transform turretBall;	
+	
+	private float nextFireTime;	
+	private float nextMoveTime;	
+	private Quaternion desiredRotation;	
+	private float aimError;
+	//Variabler end----------------------------------------------------------
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log (Time.time);
+		Debug.Log (nextMoveTime);
+		if(myTarget)
+		{
+			if(Time.time >= nextMoveTime)
+			{
+				CalculateAimPosition(myTarget.position);
+				turretBall.rotation = Quaternion.Lerp(turretBall.rotation, desiredRotation, Time.deltaTime*turnSpeed);
+			}
+			
+			if(Time.time >= nextFireTime	)
+			{
+				FireProjectile();
+			}
+		}
+	}  
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag == "Enemy")
+		{
+			nextFireTime = Time.time+(reloadTime*5f);
+			myTarget = other.gameObject.transform;
+		}
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.transform == myTarget)
+		{
+			myTarget = null;
+		}
+	}
+	
+	void CalculateAimPosition(Vector3 targetPos)
+	{
+		Vector3 aimPoint = new Vector3(targetPos.x+aimError, targetPos.y+aimError, targetPos.z+aimError);
+		desiredRotation = Quaternion.LookRotation(aimPoint);
+	}
+	
+	void CalculateAimError()
+	{
+		aimError = Random.Range(-errorAmount,errorAmount);
+	}
+	
+	void FireProjectile()
+	{
+
+		nextFireTime = Time.time+reloadTime;
+		nextMoveTime = Time.time+firePauseTime;
+		CalculateAimError();
+		
+		foreach(Transform theMuzzlePos in muzzlePositions)
+		{
+			Instantiate(myProjectile, theMuzzlePos.position, theMuzzlePos.rotation);
+			Instantiate(muzzleEffect, theMuzzlePos.position, theMuzzlePos.rotation);
+		}
+	}
+}
---
+++ projects-public/2015-TowerDefense/Assets/Script/bullet.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	public float mySpeed = 10;
+	public float myRange = 10;
+	public GameObject target;
+	public bool damageCD = false;
+
+	private float myDist;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * mySpeed);
+		myDist += Time.deltaTime * mySpeed;
+		if(myDist>= myRange)
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.tag == "Enemy")
+		{
+
+			Destroy(gameObject);
+			other.gameObject.GetComponent<Health>().Attack(1);
+
+
+		}
+	}
+
+
+}
