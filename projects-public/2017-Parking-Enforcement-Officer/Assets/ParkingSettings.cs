b12a9b2b7fc1bdc288de81a5dace1b08ae6a51b8
Mark Grøning Hansen <marjk9004@gmail.com>
2019-10-18 01:30:34 +0200

    Uploaded project

---
+++ projects-public/2017-Parking-Enforcement-Officer/Assets/ParkingSettings.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParkingSettings : MonoBehaviour {
+    public bool canGetTicket;
+
+    public bool touchingCurb;
+    public bool outOfTime;
+    public bool inBooth;
+    public bool touchingMarkedCurb;
+    public bool tooCloseToCrossSection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter (Collider col)
+    {
+        //if(col.gameObject.tag == "")
+    }
+}
